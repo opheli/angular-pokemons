35d3dc035b42ecd1f94eb86bfe7b46123c3d13f1
ophélie <ophelieleleu@yahoo.fr>
2022-11-09 16:06:47 +0100

    angular mise en pace ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,22 @@ import { Pokemon } from './pokemon';
 
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
   
   ngOnInit() {
     console.table(this.pokemonList)
   }
 
-  selectPokemon(event: MouseEvent) {
-    const index: number = +(event.target as HTMLInputElement).value;
-    console.log(`vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if(pokemon) {
+      console.log(`vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`vous avez demandé un pokémon qui n'existe pas`);
+      this.pokemonSelected = pokemon;
+    }
+
   }
   
 }
